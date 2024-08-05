cb7d5cb0cfec9780b6c418dc3aea77a68f84a005
hanshihao <hanshihao01@yuaiweiwu.com>
2024-08-05 17:28:03 +0800

    修改log配置

--- src/main/java/com/cy/store_/StoreApplication.java
+++ src/main/java/com/cy/store_/StoreApplication.java
@@ -4,11 +4,24 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 @MapperScan("com.cy.store_.mapper")
 public class StoreApplication {
 
+
+
     public static void main(String[] args) {
+        // 设置日志目录环境变量
+        String logDir1 = "/Users/hanshihao/logs/store/log";
+        String logDir2 = "/Users/hee/software/logs/store/log";
+
+        if (new File(logDir1).exists()) {
+            System.setProperty("LOG_DIR", logDir1);
+        } else {
+            System.setProperty("LOG_DIR", logDir2);
+        }
         SpringApplication.run(StoreApplication.class, args);
     }
 
